8edf07406b5239eca1b3d07b57ca8bdec698531d
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-09-13 00:55:24 -0300

    req 5: create table tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import PlanetsContext from '../context/PlanetsContext';
+import { mockContextData, mockLoadingFalse, mockPlanetsData } from './mocks';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("Testa o componente <Table />", () => {
+  test("Renderiza loading se loading estiver true", () => {
+    render(
+      <PlanetsContext.Provider value={mockLoadingFalse}>
+        <App />
+      </PlanetsContext.Provider>
+    );
+
+    const loadingMessage = screen.getByText(/Loading.../i);
+    expect(loadingMessage).toBeInTheDocument();
+  });
+  test("Renderiza a tabela com dados quando loading for false", () => {
+    render(
+      <PlanetsContext.Provider value={mockContextData}>
+        <App />
+      </PlanetsContext.Provider>
+    );
+
+    mockPlanetsData.forEach((planet) => {
+      expect(screen.getByText(planet.name)).toBeInTheDocument();
+      expect(screen.getByText(planet.rotation_period)).toBeInTheDocument();
+    });
+  });
 });
+
