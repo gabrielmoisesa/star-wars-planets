606c8f136de173fd567167b0bf3fb407f31df7d2
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-09-16 20:41:20 -0300

    style name filter & filters container

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -2,10 +2,11 @@ import ButtonClearFilters from './ButtonClearFilters/ButtonRemoveFilters';
 import NameFilter from './NameFilter/NameFilter';
 import NumericFilter from './NumericFilter/NumericFilter';
 import RenderFilter from './RenderFilterText/RenderFilterText';
+import './Filters.css';
 
 function Filters() {
   return (
-    <div>
+    <div className="filters-container">
       <NameFilter />
       <NumericFilter />
       <ButtonClearFilters />
